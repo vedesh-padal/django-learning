20132e3a0417baaed9c63dfd52452db22ed731d4
vedesh-padal <vedeshpadal@gmail.com>
2024-10-19 00:47:32 +0530

    styling improvement and store page rendering improvement

--- chai/forms.py
+++ chai/forms.py
@@ -1,5 +1,15 @@
 from django import forms
 from .models import ChaiVariety
 
+
 class ChaiVarietyForm(forms.Form):
-  chai_variety = forms.ModelChoiceField(queryset=ChaiVariety.objects.all(), label="Select Chai Variety")
+    chai_variety = forms.ModelChoiceField(
+        queryset=ChaiVariety.objects.all(),
+        label="Select Chai Variety",
+        empty_label="Choose a variety...",
+        widget=forms.Select(
+            attrs={
+                "class": "w-full p-3 border border-gray-300 rounded-lg bg-white text-gray-700 focus:outline-none focus:ring-2 focus:ring-amber-500"
+            }
+        ),
+    )
